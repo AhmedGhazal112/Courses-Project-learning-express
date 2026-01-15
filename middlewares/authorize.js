accfa311b420093702e829c508c972a18df54e59
AhmedGhazal112 <ahmedmedooo580@gmail.com>
2026-01-15 10:32:58 +0200

    modified authorization

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -1,11 +1,21 @@
 const jwt = require("jsonwebtoken");
+
 const authorize = (roles) => {
     return (req, res, next) => {
-        if (!roles.includes(req.user.role)) {
-            const error = new Error("Not Authorized!!!");
-            next(error);
+        try {
+            if (!req.user || !req.user.role) {
+                return res.status(401).json({ message: "User information is missing or invalid." });
+            }
+
+            if (!roles.includes(req.user.role)) {
+                return res.status(403).json({ message: "Access denied. You do not have the required permissions." });
+            }
+
+            next();
+        } catch (error) {
+            res.status(500).json({ message: "An error occurred during authorization.", error: error.message });
         }
-        next()
-    }
-}
+    };
+};
+
 module.exports = authorize;
